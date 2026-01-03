bd67aeb974090e1c04a40f2f721499c1b6406740
abdullahiabdikadir <abdullahiabdikadir35@gmail.com>
2026-01-03 19:49:14 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here!
+const postList = document.getElementById("post-list");
+
+async function fetchPosts() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+  displayPosts(posts);
+}
+
+function displayPosts(posts) {
+  const post = posts[0];
+
+  const title = document.createElement("h1");
+  title.textContent = post.title;
+
+  const body = document.createElement("p");
+  body.textContent = post.body;
+
+  postList.appendChild(title);
+  postList.appendChild(body);
+}
+
+fetchPosts();
+
